dbfa4b2c5e90608a287c04a1240316ae956792a1
Roman Horobets <roman.horobets@rolique.io>
2018-07-18 16:07:36 +0300

    vacancies

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -8,35 +8,29 @@ import Vacancies from '../vacancies';
 import Bithdays from '../birthday';
 import Grid from '@material-ui/core/Grid';
 
-const contentful = require('contentful');
-const client = contentful.createClient({
-  space: 'csc8avj2n586',
-  accessToken: '5378a38f7ea0188752654c779c59c42b3fdaab8e0e7b64e5deba662856f9285a'
-});
-
 class App extends Component {
 
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
+  // constructor (props) {
+  //   super(props)
+  //   this.state = {}
+  // }
 
-  componentDidMount () {
-    client.getAssets()
-      .then((response) => this.setState(
-        {
-          response: response
-        }
-      ))
-      .catch(console.error)
-  }
+  // componentDidMount () {
+  //   client.getEntries()
+  //     .then((response) => this.setState(
+  //       {
+  //         response: response
+  //       }
+  //     ))
+  //     .catch(console.error)
+  // }
 
   render() {
     return (
       <div className='App'>
-        {
+        {/* {
           !this.state ? '' : console.log(this.state.response)
-        }
+        } */}
         <Header />
         <main className='main'>
           <Grid container spacing={16}>
--- src/components/vacancies/index.js
+++ src/components/vacancies/index.js
@@ -1,57 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.css';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
+import * as contentful from 'contentful';
 
-const vacancies = [
-  'NodeJS Middle',
-  'NodeJS Junior',
-  'Angular 2 Middle',
-  'Angular 2 Junior',
-  'Team Lead (Full-stack, Node.js/Angular)',
-  'Full-stack (PHP/Angular1) Middle/Senior',
-  'Business Analyst',
-  'Project Coordinator',
-  'PM Middle',
-  'QA Middle'
-]
+const client = contentful.createClient({
+  space: 'csc8avj2n586',
+  accessToken: '5378a38f7ea0188752654c779c59c42b3fdaab8e0e7b64e5deba662856f9285a'
+});
 
-export default () => (
-  <Card className="vacancies">
-    <CardContent className="vacancies__content">
-      <Typography 
-        component='h2' 
-        variant='headline' 
-        gutterBottom  
-        color='inherit'
-      >
-        Open vacancies at Rolique
-      </Typography>
-      <Typography 
-        component='p' 
-        color='inherit'
-      >
-        This summer's gonna be super hot. Maybe even as hot as the open vacancies at Rolique:
-      </Typography>
-      <Typography 
-        component='div' 
-        color='inherit'
-      >
-        <ul className="vacancies-list global-list">
-          {
-            vacancies.map((el, key) => (
-              <li key={key}>{el}</li>
-            ))
-          }
-        </ul>
-      </Typography>
-      <Typography 
-        component='p' 
-        color='inherit'
-      >
-        PS. Feel free to refer your friends and acquaintance to Tanya, Stepan and Albina.
-      </Typography>
-    </CardContent>
-  </Card>
-)
+class Vacancies extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount () {
+    client.getEntries({content_type: 'vacancies'})
+      .then((response) => this.setState({
+        vacancies: response.items[0].fields.vacanciesList
+      }))
+      .catch(console.error)
+  }
+
+  render() {
+    return(
+      <Card className="vacancies">
+        <CardContent className="vacancies__content">
+          <Typography 
+            component='h2' 
+            variant='headline' 
+            gutterBottom  
+            color='inherit'
+          >
+            Open vacancies at Rolique
+          </Typography>
+          <Typography 
+            component='p' 
+            color='inherit'
+          >
+            This summer's gonna be super hot. Maybe even as hot as the open vacancies at Rolique:
+          </Typography>
+          <Typography 
+            component='div' 
+            color='inherit'
+          >
+            <ul className="vacancies-list global-list">
+              {
+                this.state.vacancies && this.state.vacancies.map((vacancy, i) => (
+                  <li key={i}>
+                    {vacancy}
+                  </li>
+                ))
+              }
+            </ul>
+          </Typography>
+          <Typography 
+            component='p' 
+            color='inherit'
+          >
+            PS. Feel free to refer your friends and acquaintance to Tanya, Stepan and Albina.
+          </Typography>
+        </CardContent>
+      </Card>
+    )
+  }
+}
+
+export default Vacancies;
